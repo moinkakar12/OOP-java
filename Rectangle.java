5b5461e6d06dcc6925e8e1487058cc92905d0d80
Moin Khan <115452798+moinkakar12@users.noreply.github.com>
2022-11-20 23:37:23 +0500

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,8 @@
+public class CalculateRectangle
+     {
+         public static void main(String[] args)
+         {
+             Rectangle r = new Rectangle (6 , 4 );
+             System.out.println("Area= ",  r.area());
+         }
+     }
