d63ca3554e94706b315acad05e8a7cf8034d6900
Moin Khan <115452798+moinkakar12@users.noreply.github.com>
2022-11-28 19:44:30 +0500

    Add files via upload

---
+++ Contact.java
@@ -0,0 +1,128 @@
+import java.util.HashMap;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String emailAddress;
+    private String contactId;
+    private String Address;
+    private String Website;
+    private String Birthday;
+    private String nickName;
+    private String Company;
+
+    public Contact() {
+
+    }
+
+    public Contact(String firstName, String lastName, String phoneNumber, String emailAddress, String contactId,
+                   String address, String website, String birthday, String nickName, String company) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.contactId = contactId;
+        this.Address = address;
+        this.Website = website;
+        this.Birthday = birthday;
+        this.nickName = nickName;
+        this.Company = company;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(String contactId) {
+        this.contactId = contactId;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        this.Address = address;
+    }
+
+    public String getWebsite() {
+        return Website;
+    }
+
+    public void setWebsite(String website) {
+        Website = website;
+    }
+
+    public String getBirthday() {
+        return Birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        Birthday = birthday;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getCompany() {
+        return Company;
+    }
+
+    public void setCompany(String company) {
+        Company = company;
+    }
+
+
+    @Override
+    public String toString() {
+        return firstName + lastName + phoneNumber + emailAddress + contactId + Address + Website + Birthday + nickName + Company;
+    }
+
+
+    public static void main(String[] args) {
+        Contact c = new Contact("Moin", "Khan", "+923188001027",
+                "moinkhankakar123@gmail.com", "moinkakar12", "H#2, Ramzan Colony, Ispini Road, Quetta", "www.twitter.com/moinkakar2", "03/05/2001", "Mono", "ATZ");
+        System.out.println(c);
+        //Contact c2 = new Contact();
+        //System.out.println(c2.getFirstName());
+    }
+}
+
